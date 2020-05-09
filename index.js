7c6359c06cae2cd74dbec17e2a611fea0dc7585b
replacem3nts <donny.landis@gmail.com>
2020-05-09 19:26:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, fn) {
+    let newAr = []
+    array.forEach(item => newAr.push(fn(item)))
+    return newAr
+}
+
+function reduce(array, fn, sp) {
+    let reduct
+    if(!!sp) {
+        reduct = sp
+    } else {
+        reduct = array[0]
+        array = [...array.slice(1)]
+    }
+
+    array.forEach(item => reduct = fn(item, reduct))
+    return reduct
+}
